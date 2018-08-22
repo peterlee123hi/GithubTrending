5e242d74363ed4159d4dc2617fb69d03297306cb
Peter Lee <peter.lee@berkeley.edu>
2018-08-21 17:37:05 -0700

    Added data module and project entity

---
+++ data/src/main/java/tech/peterlee/data/model/ProjectEntity.kt
@@ -0,0 +1,4 @@
+package tech.peterlee.data.model
+
+data class ProjectEntity(val id: String, val name: String, val fullName: String, val starCount: String,
+                   val dateCreated: String, val ownerName: String, val ownerAvatar: String)
